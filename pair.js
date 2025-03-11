2ae045fe65c6ab17f3ecba11c8ca868a7e170d60
chathurahansaka1 <hansakachathura4@gmail.com>
2025-03-11 07:39:55 -0400

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://telegra.ph/file/c1737376abe4978d2c556.jpg",
               },
               caption: sid,
             });
